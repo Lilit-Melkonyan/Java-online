b0c84cc0e0c3b0083fdde422853994591806c1af
Melkonyan Lilit <lilith.melkonyanl@gmail.com>
2025-06-05 23:26:18 +0400

    My first git commit

---
+++ QA/src/QA.java
@@ -0,0 +1,9 @@
+public class QA {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+
+        String name="Lilit";
+        String surname= "Melkonyan";
+        System.out.println(name+" "+surname);
+    }
+}
