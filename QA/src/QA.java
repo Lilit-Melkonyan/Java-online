72ce043505d26b86cb00330c963c98e93282d2cb
Melkonyan Lilit <lilith.melkonyanl@gmail.com>
2025-06-06 00:04:30 +0400

    change commit

--- QA/src/QA.java
+++ QA/src/QA.java
@@ -2,6 +2,11 @@ public class QA {
     public static void main(String[] args) {
         System.out.println("Hi");
 
+        byte x=127;
+        System.out.println(x);
+        System.out.println(x);
+
+
         String name="Lilit";
         String surname= "Melkonyan";
         System.out.println(name+" "+surname);
